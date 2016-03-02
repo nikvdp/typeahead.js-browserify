c47f646b7a0dbc24e84bdd74d0d2d3f6d1430761
Adam Tomat <hello@adam-tomat.co.uk>
2016-03-02 15:54:45 +0000

    Fixed dependency paths

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "Bloodhound": require("typeahead.js/dist/bloodhound.js"),
-    "loadjQueryPlugin": function() {require("typeahead.js");}
+    "Bloodhound": require("corejs-typeaheads/dist/bloodhound.js"),
+    "loadjQueryPlugin": function() {require("corejs-typeahead");}
 };
